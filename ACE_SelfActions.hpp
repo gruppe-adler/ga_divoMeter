aaee59444550a87751fd89c72de9d4322c828dc9
Salbei <karsus3@web.de>
2017-10-06 14:21:32 +0200

    Update ACE_SelfActions.hpp

--- ACE_SelfActions.hpp
+++ ACE_SelfActions.hpp
@@ -17,23 +17,3 @@ class grad_enhancedDiving_close
      priority = 2.5;
      icon = "";
 };
-
-class grad_enhancedDiving_emergencyAsent 
-{
-	displayName = $STR_grad_enhancedDiving_emergencyAsent;
-	condition = "(((eyePos player select 2) < 0) && EDGEARON)";
-	statement = "[] call grad_enhancedDiving_fnc_emergencyAscent";
-	exceptions[] = {"isNotSwimming"};
-	priority = 2.5;
-	icon = "";
-};
-/*	
-class grad_enhancedDiving_switchTank
-{
-     displayName = $STR_grad_enhancedDiving_switchTank;
-     condition = "((backpack player) in EDDOUBLE)";
-     statement = "[] call grad_enhancedDiving_fnc_switchTank";
-     priority = 2.5;
-     icon = "";
-};
-*/
