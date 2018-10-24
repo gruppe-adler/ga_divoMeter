5deb2d3c12115c8b58a361bd94b4f1dfbbfc8800
Salbei <salbei@gruppe-adler.de>
2018-10-24 10:15:09 +0100

    try

--- mod.cpp
+++ mod.cpp
@@ -8,6 +8,6 @@ logo = "logo_grad_ca.paa";
 logoOver = "logo_grad_ca.paa";
 tooltip = "Gruppe Adler Enhanced Diving";
 overview = "GRAD Enhanced Diving contains an assortment of assets and functions to enhance armas underwater experience.";
-overviewPicture = "Adler.paa";
+overviewPicture = "logo_grad_ca.paa";
 overviewText = "";
 overviewFootnote = "";
